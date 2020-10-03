8b76c4af3449fd19fbdc139aad99568811fe7679
Jason.Yang@wonderful.com <jason.yang@wonderful.com>
2020-10-03 11:21:02 -0700

    clean and build

--- public/sw.js
+++ public/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f95d12b150e1d0d49d82.js"
+    "url": "webpack-runtime-a888e6ae3e2e066a7c51.js"
   },
   {
     "url": "framework-5fc6169171e885c3b522.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "52be6810f9e9abbc88746d4ec7967b88"
+    "revision": "92b6311d00dfc5c8206ce3f8df586be6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
